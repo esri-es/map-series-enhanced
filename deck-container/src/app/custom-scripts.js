474391dc4e90248132b234a63ff3cd915dcad081
RaulJimenezOrtega <hhkaos@gmail.com>
2018-05-28 17:21:58 +0200

    Adding custom params and bg

--- deck-container/src/app/custom-scripts.js
+++ deck-container/src/app/custom-scripts.js
@@ -1,16 +1,33 @@
 define(["dojo/topic"], function(topic) {
-  /*
-   * Custom Javascript to be executed while the application is initializing goes here
-   */
-
-  // The application is ready
-  topic.subscribe("tpl-ready", function(){
     /*
-     * Custom Javascript to be executed when the application is ready goes here
-     */
+    * Custom Javascript to be executed while the application is initializing goes here
+    */
+    var getParameterByName = function(name, url) {
+        if (!url) url = window.location.href;
+        name = name.replace(/[\[\]]/g, "\\$&");
+        var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+        if (!results) return null;
+        if (!results[2]) return '';
+        return decodeURIComponent(results[2].replace(/\+/g, " "));
+    };
 
-     if(window.location.href.indexOf("hideTitle") != -1){
-         document.getElementById('headerDesktop').style.display='none';
-     }
-  });
+    // The application is ready
+    topic.subscribe("tpl-ready", function(){
+        /*
+        * Custom Javascript to be executed when the application is ready goes here
+        */
+
+        if(getParameterByName("hideTitle") != null){
+            document.getElementById('headerDesktop').style.display='none';
+        }
+        var activeSection = getParameterByName("activeSection");
+        if(activeSection != null){
+            activeSection = parseInt(activeSection);
+            app.ui.mainStage.updateMainMediaWithStoryMainMedia(activeSection,false)
+            app.ui.navBar.showEntryIndex(activeSection)
+            app.data.setCurrentSectionIndex(activeSection)
+        }
+
+    });
 });
