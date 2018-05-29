958446a4739a4772535a3aa5a04a6ae1df5de654
RaulJimenezOrtega <hhkaos@gmail.com>
2018-05-29 06:58:24 +0200

    Adding Youtube API key

--- deck-container/src/app/config.js
+++ deck-container/src/app/config.js
@@ -457,7 +457,7 @@ app.cfg = {
 	// If the application is deployed on Portal for ArcGIS or your own server, the Youtube api call
 	//  won't be perfomed until you set the following flag and provide your own key
 	YOUTUBE_DISABLE_ON_PORTAL: true,
-	YOUTUBE_API_KEY: "AIzaSyDevTFP16nz6sA-akiOVi6wWXiplJnQ4qw",
+	YOUTUBE_API_KEY: "AIzaSyBNSN0VyilMYKG5JHBtZnuYgE9x4LxH82c",
 
 	//
 	// Builder direct creation
